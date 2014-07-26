b2f7f2229ee1390084e26e04e6e7db3d1e1c3722
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2014-07-26 22:40:56 +0300

    Installs devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,19 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  admin      :boolean
-#  created_at :datetime
-#  updated_at :datetime
+#  id                  :integer          not null, primary key
+#  name                :string(255)
+#  email               :string(255)
+#  admin               :boolean
+#  created_at          :datetime
+#  updated_at          :datetime
+#  encrypted_password  :string(255)      default(""), not null
+#  remember_created_at :datetime
 #
 
 class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
   validates_presence_of :name, :email, :admin
   validates_uniqueness_of :email
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726193126) do
+ActiveRecord::Schema.define(version: 20140726193513) do
 
   create_table "banners", force: true do |t|
     t.string   "name"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 20140726193126) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
